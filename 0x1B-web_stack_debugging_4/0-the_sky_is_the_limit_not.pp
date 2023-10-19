cf9f45fb0ed0d4d3529c1e695f8afde15543153b
Imukua <ianmukuaa@gmail.com>
2023-10-19 13:35:21 +0300

    puppent lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # add  amount of traffic Nginx server can handle.
 
-#add  treffic limit
+# add  treffic limit
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
-#restart thr server
+# restart thr server
 exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
